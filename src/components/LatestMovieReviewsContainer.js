0435b96faaf307d967ca8cf4e2d3dd7064417ad5
kendallstephens <kendoll.stephens@gmail.com>
2020-11-05 10:48:45 -0800

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,30 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component{
+
+    state = {
+        reviews: []
+    }
+
+    componentDidMount() {
+        fetch('https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=HLQNnPBzvLFHZ4ip3CQ7JIL5AtORdjIS')
+        .then(res => res.json())
+        .then(res => this.setState({ 
+            reviews: res.results
+         }));
+    }
+
+    render(){
+        console.log(this.state)
+        return(
+            <div>
+                <h3>Movie Reviews:</h3>
+                <MovieReviews reviews = {this.state.reviews}/>
+            </div>
+        )
+    }
+}
+
+export default LatestMovieReviewsContainer
